73eb571ba5b3235e3a51aac218b3f7cf85a7452a
KeanuXKnight <keanuroush@gmail.com>
2016-02-09 11:05:06 -0800

    + index.Html and app.js > repo

---
+++ app.js
@@ -0,0 +1,38 @@
+// JS file for Feb 8 class demo
+
+var userName = prompt('what is your name?');
+
+console.log('User said his name is ' + userName);
+
+  alert('Hello ' + userName + ', my name is sir reginald the first, I will be interviewing you today.');
+
+ console.log('Hey, what are you looking at');
+
+  var pickedNum = prompt('Pick a number between one and ten.');
+
+  alert('Fool! My legend begins in the 19th century');
+
+
+var pickedAn = prompt('What is your favorite Creature?');
+
+alert('My favorite is myself of course.');
+console.log('I like fish!.');
+
+ var userName = prompt('what was your name again?');
+
+ console.log('User said his name is ' + userName);
+
+
+ alert('I do not like it, not one bit.');
+ console.log('I like it, ' + userName + '!');
+
+alert('You know what. I think I will just call you Sir ' + pickedAn + ' for now on.')
+
+prompt( 'Sir ' + pickedAn + ", you said you where " + pickedNum + ' years old right?');
+
+alert('Of course you did, after all I am always right,');
+
+alert('Well thats it for questions today. My employer dicided not to hire you, because he hates you...      and thinks your name is stupid.');
+confirm('Good bye, Sir ' + pickedAn + '. Maybe next time. ');
+alert('Why are you still here! You should have pressed cancel, fool. Go away');
+console.log('well that was awkward!');
